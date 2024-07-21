4921918e7b7e9fc3025aa1c95a75b078ee976ab6
sahilsrivastava25 <sahilsrivastava827@gmail.com>
2024-07-21 16:05:22 +0530

    backend created

---
+++ backend/controller/redis.controller.js
@@ -0,0 +1,10 @@
+// const Redis=require('ioredis')
+// const redis=new Redis({
+//     host:"redis-18282.c321.us-east-1-2.ec2.cloud.redislabs.com",
+//     port:"18282",
+//     password:"DFmC9wlmymAykcLymPKaWAwdrQQJyin0"
+// })
+
+// module.exports={
+//     redis
+// }
